de86044cb82824bac9f63396f514432d82bb9e9d
Michał Kuźba <kmichael00968@gmail.com>
2017-01-23 19:06:51 +0100

    Some functions implemented

--- tree.h
+++ tree.h
@@ -14,17 +14,19 @@ class Tree {
     using REF_NODE = std::shared_ptr<Tree<T>>;
     REF_NODE _left, _right;
     T _value;
+    bool _hasValue = 1;
     Tree(T value, REF_NODE left, REF_NODE right) : _left(left), _right(right), _value(value) {}
     Tree(T value): _value(value) {}
 public:
-    Tree() {}
+    Tree() : _hasValue(0) {}
     Tree(REF_NODE root) : _left(root->_left), _right(root->_right), _value(root->_value) {}
 
-    T fold(std::function<T (T, T, T)> operation,T init) {
-        if (this == nullptr)
+    template <typename R, typename F>
+    R fold(F operation, R init) {
+        if (this == nullptr || !_hasValue)
             return init;
         else
-            return operation(_left->_value, _right->_value, _value);
+            return operation(_value, _left->fold(operation, init), _right->fold(operation, init));
     };
 
     static REF_NODE createEmptyNode() {
@@ -37,6 +39,40 @@ public:
 
     static REF_NODE createValueNode(T value, REF_NODE left, REF_NODE right) {
         return REF_NODE(new Tree(value, left, right));
+    }
+
+    unsigned height() {
+        return fold<unsigned>([&](unsigned val, unsigned l_height, unsigned r_height){ return std::max(l_height, r_height) + 1; }, 0);
+    }
+
+    unsigned size() {
+        return fold<unsigned>([&](unsigned val, unsigned l_size, unsigned r_size) { return l_size + r_size + unsigned(_hasValue); }, 0);
+    }
+
+    /**
+     * max, min, is_bst, is_node_with_value
+     */
+    using NODE_INFO = std::tuple<T, T, bool, bool>;
+
+    bool is_bst() {
+        return std::get<2>(fold([](T val, NODE_INFO l_tree, NODE_INFO r_tree){
+            bool l_bst = std::get<2>(l_tree), r_bst = std::get<2>(r_tree);
+
+            T l_max = std::get<0>(l_tree), l_min = std::get<1>(l_tree),
+                r_max = std::get<0>(r_tree), r_min = std::get<1>(r_tree);
+
+            bool left_valid = std::get<3>(l_tree);
+            bool right_valid = std::get<3>(r_tree);
+
+            if (!left_valid)
+                l_max = l_min = val;
+            if (!right_valid)
+                r_max = r_min = val;
+
+            bool node_bst = (val >= l_max) && (val <= r_min);
+
+            return std::make_tuple(std::max(val, r_max), std::min(val, l_min), l_bst && r_bst && node_bst, true);
+        }, std::make_tuple(0, 0, true, false)));
     }
 
 
